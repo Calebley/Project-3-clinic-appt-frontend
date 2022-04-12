14610bcfcd9ca34645218964c55ecf0f56ca1919
Caleb <calebley@hotmail.com>
2022-04-12 19:46:57 +0800

    created authentication token

--- src/components/UserSignup.jsx
+++ src/components/UserSignup.jsx
@@ -1,26 +1,53 @@
-import React from "react"
+import React, { useState } from "react"
+import { Navigate } from "react-router-dom"
+// import { register }
 
-const UserSignup = () => {
+const UserSignup = ({ isUserAuthenticated }) => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
+    })
+
+    const { name, email, password, password2 } = formData
+    const onChange = e => setFormData({
+        ...formData,
+        [e.target.name]: e.target.value
+    })
+
+    const onSubmit = async e => {
+        e.preventDefault()
+        if(password === password2) {
+            alert("Password does not match")
+        } else {
+            register({name, email, password })
+        }
+    }
+    if (isUserAuthenticated) {
+        return <Navigate to="/" />
+    }
+
     return(
         <div className="container">
             <h1>Sign Up page</h1>
             <div className="signupform">
-                <form>
+                <form onSubmit={e => onSubmit(e)}>
                     <div className="form-group">
                     <label className="label" for="exampleInputEmail">Email address:</label>
-                    <input type="email" className="form-control" placeholder="Enter your email address" name="email" />
+                    <input type="email" className="form-control" placeholder="Enter your email address" name="email" value={email} onChange={e => onChange(e)}/>
                     </div>
                     <div className="form-group">
                     <label className="label" for="exampleInputEmail1">Full name:</label>
-                    <input type="text" className="form-control" placeholder="Enter your full name" name="name" />
+                    <input type="text" className="form-control" placeholder="Enter your full name" name="name" value={name} onChange={e => onChange(e)} />
                     </div>
                     <div className="form-group">
                     <label className="label" for="exampleInputPassword1">Password:</label>
-                    <input type="password" className="form-control" placeholder="Enter password" name="password" />
+                    <input type="password" className="form-control" placeholder="Enter password" name="password" value={password} onChange={e => onChange(e)} />
                     </div>
                     <div className="form-group">
                     <label className="label" for="exampleInputEmail1">Confirm password:</label>
-                    <input type="text" className="form-control" placeholder="Enter password again" name="name" />
+                    <input type="text" className="form-control" placeholder="Enter password again" name="password2" value={password2} onChange={e => onChange(e)} />
                     </div>
                     <input type="submit" value="Sign up" />
                 </form>
